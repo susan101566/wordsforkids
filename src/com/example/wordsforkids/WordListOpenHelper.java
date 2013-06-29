cf507ef99c00ff27507564e2045d4e044e26772e
jjw1707 <jjw1707@gmail.com>
2013-06-29 03:06:08 -0700

    Using the database in student word list.

--- src/com/example/wordsforkids/WordListOpenHelper.java
+++ src/com/example/wordsforkids/WordListOpenHelper.java
@@ -109,10 +109,11 @@ public class WordListOpenHelper extends SQLiteOpenHelper {
 		String countQuery = "SELECT  * FROM " + TABLE_PHOTOS;
 		SQLiteDatabase db = this.getReadableDatabase();
 		Cursor cursor = db.rawQuery(countQuery, null);
+		int count = cursor.getCount();
 		cursor.close();
 
 		// return count
-		return cursor.getCount();
+		return count;
 	}
 	
 	// Updating single photo
