e237216b03c87195be1cea4cc208cbcf58f85149
Susan Wang <susanwang1015@gmail.com>
2013-06-29 15:03:34 -0700

    new button

--- gen/com/example/wordsforkids/R.java
+++ gen/com/example/wordsforkids/R.java
@@ -28,22 +28,11 @@ public final class R {
     public static final class drawable {
         public static final int delete_button=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int sample_0=0x7f020002;
-        public static final int sample_1=0x7f020003;
-        public static final int sample_10=0x7f020004;
-        public static final int sample_11=0x7f020005;
-        public static final int sample_2=0x7f020006;
-        public static final int sample_3=0x7f020007;
-        public static final int sample_4=0x7f020008;
-        public static final int sample_5=0x7f020009;
-        public static final int sample_6=0x7f02000a;
-        public static final int sample_7=0x7f02000b;
-        public static final int sample_8=0x7f02000c;
-        public static final int sample_9=0x7f02000d;
-        public static final int student_title=0x7f02000e;
-        public static final int submit_button=0x7f02000f;
-        public static final int teacher_title=0x7f020010;
-        public static final int title=0x7f020011;
+        public static final int new_button=0x7f020002;
+        public static final int student_title=0x7f020003;
+        public static final int submit_button=0x7f020004;
+        public static final int teacher_title=0x7f020005;
+        public static final int title=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f09000d;
