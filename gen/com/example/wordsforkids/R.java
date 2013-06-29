3cc8cf0212cbee788443cb4e4758dca162b21140
Susan Wang <susanwang1015@gmail.com>
2013-06-29 14:59:02 -0700

    delete_button.png

--- gen/com/example/wordsforkids/R.java
+++ gen/com/example/wordsforkids/R.java
@@ -41,8 +41,9 @@ public final class R {
         public static final int sample_8=0x7f02000c;
         public static final int sample_9=0x7f02000d;
         public static final int student_title=0x7f02000e;
-        public static final int teacher_title=0x7f02000f;
-        public static final int title=0x7f020010;
+        public static final int submit_button=0x7f02000f;
+        public static final int teacher_title=0x7f020010;
+        public static final int title=0x7f020011;
     }
     public static final class id {
         public static final int action_settings=0x7f09000d;
