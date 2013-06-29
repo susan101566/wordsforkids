1dd512630325224afb062ea58d3d755a2cdd91cb
Susan Wang <susanwang1015@gmail.com>
2013-06-29 14:32:53 -0700

    title.png

--- gen/com/example/wordsforkids/R.java
+++ gen/com/example/wordsforkids/R.java
@@ -41,6 +41,7 @@ public final class R {
         public static final int sample_9=0x7f02000c;
         public static final int student_title=0x7f02000d;
         public static final int teacher_title=0x7f02000e;
+        public static final int title=0x7f02000f;
     }
     public static final class id {
         public static final int action_settings=0x7f09000d;
@@ -76,14 +77,14 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050006;
-        public static final int answer=0x7f05000d;
+        public static final int answer=0x7f05000c;
         public static final int app_name=0x7f050000;
         public static final int delete=0x7f05000f;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
         public static final int new_word=0x7f050009;
-        public static final int play=0x7f05000c;
-        public static final int record=0x7f05000e;
+        public static final int play=0x7f05000e;
+        public static final int record=0x7f05000d;
         public static final int student=0x7f050003;
         public static final int submit=0x7f05000b;
         public static final int teacher=0x7f050004;
